c7878f93d7914f0981af9a35c31b2a69c499e821
Tan Qin Hui <destqh@hotmail.com>
2018-02-14 08:56:16 +0800

    recitfy echo out of bound bug and add spaces between echo args in display

--- src/sg/edu/nus/comp/cs4218/impl/app/EchoApplication.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/EchoApplication.java
@@ -44,9 +44,9 @@ public class EchoApplication implements Application {
 				stdout.write("\n\n".getBytes());
 			} else {
 				for (int i = 0; i < args.length - 1; i++) {
-					stdout.write(args[i].getBytes());
+					stdout.write((args[i] + " ").getBytes());
 				}
-				stdout.write(args[args.length].getBytes());
+				stdout.write(args[args.length - 1].getBytes());
 				stdout.write("\n".getBytes());
 			}
 		} catch (IOException e) {
