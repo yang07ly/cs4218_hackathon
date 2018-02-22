41c5f2cf6d9c86f5614a9f80b7af735971d513c0
Tan Qin Hui <destqh@hotmail.com>
2018-02-22 15:22:22 +0800

    sed interface method to throw exception for null or emtpy string

--- src/sg/edu/nus/comp/cs4218/impl/app/SedApplication.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/SedApplication.java
@@ -1,7 +1,6 @@
 package sg.edu.nus.comp.cs4218.impl.app;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -45,7 +44,9 @@ import sg.edu.nus.comp.cs4218.exception.SedException;
  * </p>
  */
 public class SedApplication implements SedItf {
-	
+
+	public static final String EXP_NULL_POINTER = "Null Pointer Exception";
+
 	/**
 	 * Runs the cat application with the specified arguments.
 	 * 
@@ -68,47 +69,28 @@ public class SedApplication implements SedItf {
 	public void run(String[] args, InputStream stdin, OutputStream stdout) 
 			throws SedException {
 		if (stdout == null) {
-			throw new SedException("Null Pointer Exception");
+			throw new SedException(EXP_NULL_POINTER);
 		}
-		
+
 		if (args == null || args.length == 0) {
 			throw new SedException("REPLACEMENT is not specified");
 		}
-		
+
 		validateReplOptions(args[0]);
 		char sepChar = getSepChar(args[0]);
 		String regexp = getRegExp(args[0], sepChar);
 		String replexp = getReplExp(args[0], sepChar);
 		int matchIndex = getNthValue(args[0], sepChar);
-		
+
 		String outputStr = "";
 		if (args.length > 1) {
-			Path filePath;
-			Path currentDir = Paths.get(Environment.currentDirectory);
 			for (int i = 1; i < args.length; i++) {
-				try {
-					filePath = currentDir.resolve(args[i]);
-				} catch (InvalidPathException e) {
-					throw new SedException("can't read " + args[i] + ": No such file or directory");
-				}
-
-				if (args[i].isEmpty() || !Files.exists(filePath) || !Files.isReadable(filePath)) {
-					throw new SedException("can't read " + args[i] + ": No such file or directory");
-				}
-
-				if (Files.isDirectory(filePath)) {
-					throw new SedException("read error on " + args[i] + ": Is a directory");
-				}
-				
 				outputStr += replaceSubstringInFile(regexp, replexp, matchIndex, args[i]);
 			}
 		} else {
-			if (stdin == null) {
-				throw new SedException("Null Pointer Exception");
-			}
 			outputStr += replaceSubstringInStdin(regexp, replexp, matchIndex, stdin);
 		}
-		
+
 		try {
 			stdout.write(outputStr.getBytes());
 		} catch (IOException e) {
@@ -131,28 +113,46 @@ public class SedApplication implements SedItf {
 	 * 				Integer specifying the index of the matched
 	 * 				substring to be replaced (default is 0).
 	 * @param fileName 
-	 * 				String specifying name of the file.
+	 * 				String specifying name of a valid file.
 	 * @throws SedException
-	 * 				If an I/O exception occurs.
+	 * 				If pattern is null or empty or
+	 * 				if replacement is null or
+	 * 				if fileName is null or 
+	 * 				if replacementIndex is 0 or
+	 * 				if an I/O exception occurs.
 	 */
 	@Override
 	public String replaceSubstringInFile(String pattern, String replacement, int replacementIndex, String fileName)
 			throws SedException {
-		String outputStr = "";
-		
-		File dir = new File(fileName);
-		if (!dir.isAbsolute()) {
-			dir = new File(Environment.currentDirectory + "/" + fileName);
+		if (fileName == null) {
+			throw new SedException(EXP_NULL_POINTER);
 		}
-		
+
+		Path filePath;
+		Path currentDir = Paths.get(Environment.currentDirectory);
 		try {
-			BufferedReader content = new BufferedReader(new FileReader(dir));
+			filePath = currentDir.resolve(fileName);
+		} catch (InvalidPathException e) {
+			throw new SedException("can't read " + fileName + ": No such file or directory");
+		}
+
+		if (fileName.isEmpty() || !Files.exists(filePath) || !Files.isReadable(filePath)) {
+			throw new SedException("can't read " + fileName + ": No such file or directory");
+		}
+
+		if (Files.isDirectory(filePath)) {
+			throw new SedException("read error on " + fileName + ": Is a directory");
+		}
+
+		String outputStr = "";	
+		try {
+			BufferedReader content = new BufferedReader(new FileReader(filePath.toFile()));
 			String line;
 			while((line = content.readLine()) != null) {
-			    outputStr += getReplacedLine(pattern, replacement, replacementIndex, line);
+				outputStr += getReplacedLine(pattern, replacement, replacementIndex, line);
 			}
 			content.close();
-			
+
 		} catch (IOException e) {
 			throw new SedException("IOException: " + e);
 		}
@@ -176,27 +176,35 @@ public class SedApplication implements SedItf {
 	 * @param stdin 
 	 * 				InputStream containing arguments from Stdin.
 	 * @throws SedException
-	 * 				If an I/O exception occurs.
+	 * 				If pattern is null or empty or
+	 * 				if replacement is null or
+	 * 				if fileName is null or 
+	 * 				if replacementIndex is 0 or
+	 * 				if an I/O exception occurs.
 	 */
 	@Override
 	public String replaceSubstringInStdin(String pattern, String replacement, int replacementIndex, InputStream stdin)
 			throws SedException {
+		if (stdin == null) {
+			throw new SedException(EXP_NULL_POINTER);
+		}
+
 		String outputStr = "";
-		
+
 		InputStreamReader inStream = new InputStreamReader(stdin);
 		try {
 			BufferedReader content = new BufferedReader(inStream);
 			String line;
 			while((line = content.readLine()) != null) {
-			    outputStr += getReplacedLine(pattern, replacement, replacementIndex, line);
+				outputStr += getReplacedLine(pattern, replacement, replacementIndex, line);
 			}
-			
+
 		} catch (IOException e) {
 			throw new SedException("IOException");
 		}
 		return outputStr;
 	}
-	
+
 	/**
 	 * Validate that the Sed options and throw SedException when error occurs.
 	 * @param option
@@ -213,13 +221,13 @@ public class SedApplication implements SedItf {
 		if (option.length() < 2) {
 			throw new SedException("char 1: unterminated: '" + option.charAt(0) + "'");
 		}
-		
+
 		char sepChar = option.charAt(1);	
 		if (!option.matches("s" + sepChar + ".*" + sepChar + ".*" + sepChar + ".*")) {
 			throw new SedException("char " + option.length() + ": unterminated: '" + option.charAt(0) + "'");
 		}	
 	}
-	
+
 	/**
 	 * Return the separating character in the validated option.
 	 * @param option
@@ -230,7 +238,7 @@ public class SedApplication implements SedItf {
 	private char getSepChar(String option) {
 		return option.charAt(1);
 	}
-	
+
 	/**
 	 * Return the regular expression in the validated option.
 	 * @param option
@@ -246,13 +254,13 @@ public class SedApplication implements SedItf {
 			throws SedException {
 		int endIndex = option.indexOf(sepChar, 2);
 		String regexp = option.substring(2, endIndex);
-		
+
 		if (regexp.isEmpty()) {
 			throw new SedException("char 0: no previous regular expression");
 		}
 		return regexp;
 	}
-	
+
 	/**
 	 * Return the replacement expression in the validated option.
 	 * @param option
@@ -267,7 +275,7 @@ public class SedApplication implements SedItf {
 		int endIndex = option.indexOf(sepChar, beginIndex);
 		return option.substring(beginIndex, endIndex);
 	}
-	
+
 	/**
 	 * Return the value of the Nth in the validated option.
 	 * @param option
@@ -283,7 +291,7 @@ public class SedApplication implements SedItf {
 			throws SedException {
 		int beginIndex = option.lastIndexOf(sepChar);
 		String nthStr = option.substring(beginIndex + 1);
-		
+
 		int matchIndex;
 		if (nthStr.isEmpty()) {
 			matchIndex = -1;
@@ -293,20 +301,20 @@ public class SedApplication implements SedItf {
 			}
 			matchIndex = Integer.parseInt(nthStr);
 		}
-		
+
 		if (matchIndex == 0) {
 			throw new SedException("char " + beginIndex + ": number option to '" + option.charAt(0) 
-									+ "' command may not be zero");
+			+ "' command may not be zero");
 		}
 		return matchIndex;
 	}
-	
+
 	/**
 	 * Returns string of the line with the matched substring replaced. 
 	 * @param pattern 
 	 * 				String specifying a regular expression in JAVA format.
 	 * @param replacement 
-	 * 				String to replace the matched pattern
+	 * 				String to replace the matched pattern.
 	 * @param replacementIndex 
 	 * 				Integer specifying the index of the matched substring to be 
 	 * 				replaced (default is 0).
@@ -314,8 +322,23 @@ public class SedApplication implements SedItf {
 	 * 				String of the line to be replaced.
 	 * @return String
 	 * 				Line with the matched substring replaced.
+	 * @throws SedException
+	 * 				If pattern is empty or the Nth value is 0.
 	 */
-	private String getReplacedLine(String pattern, String replacement, int replacementIndex, String line) {
+	private String getReplacedLine(String pattern, String replacement, int replacementIndex, String line)
+			throws SedException{
+		if (pattern == null || replacement == null || line == null) {
+			throw new SedException(EXP_NULL_POINTER);
+		}
+
+		if (pattern.length() == 0) {
+			throw new SedException("regular expression cannot be empty");
+		}
+
+		if (replacementIndex == 0) {
+			throw new SedException("command may not be zero");
+		}
+
 		if (replacementIndex < 0) {
 			return line.replaceAll(pattern, replacement) + "\n";
 		}
@@ -324,11 +347,11 @@ public class SedApplication implements SedItf {
 		for(int i = 0; i < replacementIndex; i++) {
 			matcher.find();
 		}
-		
-		if (replacementIndex == 0 || matcher.hitEnd()) {
+
+		if (matcher.hitEnd()) {
 			return line + "\n";
 		}
-		
+
 		int matchedIndex;
 		try {
 			matchedIndex = matcher.start();
