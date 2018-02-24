9186fa77aa07b59d94b6d492c273409b99b37da9
Musa <ghurabah93@hotmail.com>
2018-02-24 10:26:21 +0800

    add commands unit test

---
+++ test/sg/edu/nus/comp/cs4218/impl/cmd/CallCommandTest.java
@@ -0,0 +1,14 @@
+package sg.edu.nus.comp.cs4218.impl.cmd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CallCommandTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
