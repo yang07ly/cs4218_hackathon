274110b85027724b2fa962fd717b67cba183b4fa
Musa <ghurabah93@hotmail.com>
2018-02-24 10:34:07 +0800

    add cmd sub and io redir test files

---
+++ test/sg/edu/nus/comp/cs4218/impl/cmd/CommandSubstitutionTest.java
@@ -0,0 +1,14 @@
+package sg.edu.nus.comp.cs4218.impl.cmd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CommandSubstitutionTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
