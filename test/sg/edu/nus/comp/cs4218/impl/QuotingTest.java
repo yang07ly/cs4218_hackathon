f1f6482f1c02516e7d47db1975b9d9dbb7001c83
Unknown <mdirfanpm@gmail.com>
2018-02-25 01:29:47 +0800

    update stub
    still incomplete

--- test/sg/edu/nus/comp/cs4218/impl/QuotingTest.java
+++ test/sg/edu/nus/comp/cs4218/impl/QuotingTest.java
@@ -2,18 +2,29 @@ package sg.edu.nus.comp.cs4218.impl;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import sg.edu.nus.comp.cs4218.impl.app.EchoApplication;
+
 public class QuotingTest {
+	
+	EchoApplication echoApp;
+	String expected, result;
+	OutputStream stdout;
 
 	@Before
 	public void setUp() throws Exception {
+		echoApp = new EchoApplication();
+		stdout = new ByteArrayOutputStream();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void DoubleQuoteTest() {
+		
 	}
 
 }
