a04a110948116cbfc127e7f825e5b457562595e8
bthjehiel <jehielmisc@gmail.com>
2018-02-27 00:44:03 +0800

    Added quote command skeleton and tests

---
+++ src/sg/edu/nus/comp/cs4218/impl/cmd/QuoteCommand.java
@@ -0,0 +1,19 @@
+package sg.edu.nus.comp.cs4218.impl.cmd;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
+import sg.edu.nus.comp.cs4218.exception.ShellException;
+
+public class QuoteCommand{
+	
+	public QuoteCommand(String cmdString) {
+		
+	}
+
+	public void evaluate(InputStream stdin, OutputStream stdout) throws AbstractApplicationException, ShellException {
+		// TODO Auto-generated method stub
+		
+	}
+}
