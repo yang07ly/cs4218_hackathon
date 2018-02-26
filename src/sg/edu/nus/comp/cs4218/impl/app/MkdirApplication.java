01116f9656de454546621dff76dec1d1a7ece1cc
Tan Qin Hui <destqh@hotmail.com>
2018-02-27 02:09:09 +0800

    commented mkdir public methods

--- src/sg/edu/nus/comp/cs4218/impl/app/MkdirApplication.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/MkdirApplication.java
@@ -14,12 +14,40 @@ import sg.edu.nus.comp.cs4218.app.MkdirInterface;
 import sg.edu.nus.comp.cs4218.exception.MkdirException;
 
 public class MkdirApplication implements MkdirInterface {
-
+	
+	/**
+	 * Create folder from the given folder names. Do nothing
+	 * if folder already exists. If folder name is a path 
+	 * format, create the folder that satisfies the path
+	 * specification.
+	 * @param folderName 
+	 * 				Array of string of folder names to
+	 * 				be created.
+	 * @throws MkdirException
+	 * 				If no args is specified or
+	 * 				if the file/folder name is empty or
+	 * 				if the folder already exists or	
+	 * 				if the path do not exist.		
+	 */
 	@Override
 	public void run(String[] args, InputStream stdin, OutputStream stdout) throws MkdirException {
 		createFolder(args);
 	}
 
+	/**
+	 * Create folder from the given folder names. Do nothing
+	 * if folder already exists. If folder name is a path 
+	 * format, create the folder that satisfies the path
+	 * specification.
+	 * @param folderName 
+	 * 				Array of string of folder names to
+	 * 				be created.
+	 * @throws MkdirException
+	 * 				If no foldername is specified or
+	 * 				if the file/folder name is empty or
+	 * 				if the folder already exists or	
+	 * 				if the path do not exist.		
+	 */
 	@Override
 	public void createFolder(String... folderName) throws MkdirException {
 		if (folderName == null || folderName.length == 0) {
