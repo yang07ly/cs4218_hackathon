7b495cc629c39d1993f8ad0d667e3a2cb4eb0313
Tan Qin Hui <destqh@hotmail.com>
2018-03-22 23:26:18 +0800

    modified glob test to use CommandString.

--- unit_test/sg/edu/nus/comp/cs4218/impl/optr/GlobOperatorTest.java
+++ unit_test/sg/edu/nus/comp/cs4218/impl/optr/GlobOperatorTest.java
@@ -12,11 +12,11 @@ import org.junit.rules.ExpectedException;
 import sg.edu.nus.comp.cs4218.Environment;
 import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
 import sg.edu.nus.comp.cs4218.exception.ShellException;
-import sg.edu.nus.comp.cs4218.impl.ShellStub;
+import sg.edu.nus.comp.cs4218.impl.commons.CommandString;
 import sg.edu.nus.comp.cs4218.impl.optr.GlobOperator;
 
 public class GlobOperatorTest {
-	
+
 	private static final String TEST_DIR = System.getProperty("user.dir") + File.separator + "test_system" + File.separator + "glob_test_system";
 	
 	private static final String FILE1 = "file1.txt";
@@ -39,9 +39,12 @@ public class GlobOperatorTest {
 	private static final String ABS_FILE2 = TEST_DIR + File.separator + FILE2;
 	private static final String ABS_FOLDER1 = TEST_DIR + File.separator + FOLDER1;
 	private static final String ABS_FOLDER2 = TEST_DIR + File.separator + FOLDER2;	
+	
+	private static final String STR_SPACE = " ";
+	private static final String FILE_WILDCARD = "file*";
 
 	private GlobOperator globOptr;
-	private String[] input, output, expected;
+	private CommandString cmd, expected;
 	
 	@Rule
     public ExpectedException thrown = ExpectedException.none();
@@ -49,153 +52,180 @@ public class GlobOperatorTest {
 	@Before
 	public void setUp() throws Exception {
 		Environment.currentDirectory = TEST_DIR;
-		globOptr = new GlobOperator(new ShellStub());
-		input = output = expected = new String[0];
+		globOptr = new GlobOperator();
+		cmd = new CommandString();
+		expected = new CommandString();
 	}
 
 	@Test
 	public void testGlobEmpty() throws AbstractApplicationException, ShellException {
-		expected = new String[] {FILE_EMPTY};
-		input = new String[] {FILE_EMPTY};
-		output = globOptr.evaluate(input);
-		assertArrayEquals(expected, output);
+		expected = new CommandString(FILE_EMPTY);
+		cmd = new CommandString(FILE_EMPTY);
+		globOptr.evaluate(cmd);
+		assertEquals(expected, cmd);
 	}
 
 	@Test
 	public void testGlobMutipleSpaces() throws AbstractApplicationException, ShellException {
-		expected = new String[] {FILE_SPACES};
-		input = new String[] {FILE_SPACES};
-		output = globOptr.evaluate(input);
-		assertArrayEquals(expected, output);
+		expected = new CommandString(FILE_SPACES);
+		cmd = new CommandString(FILE_SPACES);
+		globOptr.evaluate(cmd);
+		assertEquals(expected, cmd);
 	}
 
 	@Test
 	public void testGlobExistingFile() throws AbstractApplicationException, ShellException {
-		expected = new String[] {FILE1};
-		input = new String[] {FILE1};
-		output = globOptr.evaluate(input);
-		assertArrayEquals(expected, output);
+		expected = new CommandString(FILE1);
+		cmd = new CommandString(FILE1);
+		globOptr.evaluate(cmd);
+		assertEquals(expected, cmd);
 	}
 
 	@Test
 	public void testGlobNonExistingFile() throws AbstractApplicationException, ShellException {
-		expected = new String[] {FILE_NONEXISTENT};
-		input = new String[] {FILE_NONEXISTENT};
-		output = globOptr.evaluate(input);
-		assertArrayEquals(expected, output);
+		expected = new CommandString(FILE_NONEXISTENT);
+		cmd = new CommandString(FILE_NONEXISTENT);
+		globOptr.evaluate(cmd);
+		assertEquals(expected, cmd);
 	}
 
 	@Test
 	public void testGlobEverythingRelative() throws AbstractApplicationException, ShellException {
-		expected = new String[] {FILE_W_SPACES, FILE1, FILE2, FOLDER1, FOLDER2};
-		input = new String[] {"*"};
-		output = globOptr.evaluate(input);
-		assertArrayEquals(expected, output);
+		expected = getExpectedCmdStr(FILE_W_SPACES, FILE1, FILE2, FOLDER1, FOLDER2);
+		cmd = new CommandString("*");
+		globOptr.evaluate(cmd);
+		assertEquals(expected, cmd);
 	}
 
 	@Test
 	public void testGlobEverythingAbsolute() throws AbstractApplicationException, ShellException {
-		expected = new String[] {ABS_FILE_W_SPACES, ABS_FILE1, ABS_FILE2, ABS_FOLDER1, ABS_FOLDER2};
-		input = new String[] {Environment.currentDirectory + File.separator + "*"};
-		output = globOptr.evaluate(input);
-		assertArrayEquals(expected, output);
+		expected = getExpectedCmdStr(ABS_FILE_W_SPACES, ABS_FILE1, ABS_FILE2, ABS_FOLDER1, ABS_FOLDER2);
+		cmd = new CommandString(Environment.currentDirectory + File.separator + "*");
+		globOptr.evaluate(cmd);
+		assertEquals(expected, cmd);
 	}
 	
 	@Test
 	public void testGlobEverythingWithEmptyParent() throws AbstractApplicationException, ShellException {
-		expected = new String[] {ABS_FILE_W_SPACES, ABS_FILE1, ABS_FILE2, ABS_FOLDER1, ABS_FOLDER2};
-		input = new String[] {Environment.currentDirectory + File.separator + File.separator + "*"};
-		output = globOptr.evaluate(input);
-		assertArrayEquals(expected, output);
+		expected = getExpectedCmdStr(ABS_FILE_W_SPACES, ABS_FILE1, ABS_FILE2, ABS_FOLDER1, ABS_FOLDER2);
+		cmd = new CommandString(Environment.currentDirectory + File.separator + File.separator + "*");
+		globOptr.evaluate(cmd);
+		assertEquals(expected, cmd);
 	}
 
 	@Test
 	public void testGlobAllFolders() throws AbstractApplicationException, ShellException {
-		expected = new String[] {FOLDER1, FOLDER2};
-		input = new String[] {"*/"};
-		output = globOptr.evaluate(input);
-		assertArrayEquals(expected, output);
+		expected = getExpectedCmdStr(FOLDER1, FOLDER2);
+		cmd = new CommandString("*/");
+		globOptr.evaluate(cmd);
+		assertEquals(expected, cmd);
 	}
 
 	@Test
 	public void testGlobAtFront() throws AbstractApplicationException, ShellException {
-		expected = new String[] {FILE_W_SPACES, FILE1, FILE2};
-		input = new String[] {"*.txt"};
-		output = globOptr.evaluate(input);
-		assertArrayEquals(expected, output);
+		expected = getExpectedCmdStr(FILE_W_SPACES, FILE1, FILE2);
+		cmd = new CommandString("*.txt");
+		globOptr.evaluate(cmd);
+		assertEquals(expected, cmd);
 	}
 
 	@Test
 	public void testGlobAtEnd() throws AbstractApplicationException, ShellException {
-		expected = new String[] {FILE_W_SPACES, FILE1, FILE2, FOLDER1, FOLDER2};
-		input = new String[] {"f*"};
-		output = globOptr.evaluate(input);
-		assertArrayEquals(expected, output);
+		expected = getExpectedCmdStr(FILE_W_SPACES, FILE1, FILE2, FOLDER1, FOLDER2);
+		cmd = new CommandString("f*");
+		globOptr.evaluate(cmd);
+		assertEquals(expected, cmd);
 	}
 
 	@Test
 	public void testGlobAtMiddle() throws AbstractApplicationException, ShellException {
-		expected = new String[] {FILE_W_SPACES, FILE1, FILE2, };
-		input = new String[] {"f*.txt"};
-		output = globOptr.evaluate(input);
-		assertArrayEquals(expected, output);
+		expected = getExpectedCmdStr(FILE_W_SPACES, FILE1, FILE2);
+		cmd = new CommandString("f*.txt");
+		globOptr.evaluate(cmd);
+		assertEquals(expected, cmd);
 	}
 
 	@Test
 	public void testGlobParentDirectory() throws AbstractApplicationException, ShellException {
-		expected = new String[] {FOLDER1_FILE1, FOLDER2_FILE1};
-		input = new String[] {"*" + File.separator + FILE1};
-		output = globOptr.evaluate(input);
-		assertArrayEquals(expected, output);
+		expected = getExpectedCmdStr(FOLDER1_FILE1, FOLDER2_FILE1);
+		cmd = new CommandString("*" + File.separator + FILE1);
+		globOptr.evaluate(cmd);
+		assertEquals(expected, cmd);
 	}
 
 	@Test
 	public void testGlobMultipleInSingleFolder() throws AbstractApplicationException, ShellException {
-		expected = new String[] {FILE1, FOLDER1};
-		input = new String[] {"*1*"};
-		output = globOptr.evaluate(input);
-		assertArrayEquals(expected, output);
+		expected = getExpectedCmdStr(FILE1, FOLDER1);
+		cmd = new CommandString("*1*");
+		globOptr.evaluate(cmd);
+		assertEquals(expected, cmd);
 	}
 
 	@Test
 	public void testGlobMultipleInMutipleFolder() throws AbstractApplicationException, ShellException {
-		expected = new String[] {FOLDER1_FILE1, FOLDER1_FILE2, FOLDER2_FILE1, FOLDER2_FILE2};
-		input = new String[] {"*" + File.separator + "file*"};
-		output = globOptr.evaluate(input);
-		assertArrayEquals(expected, output);
+		expected = getExpectedCmdStr(FOLDER1_FILE1, FOLDER1_FILE2, FOLDER2_FILE1, FOLDER2_FILE2);
+		cmd = new CommandString("*" + File.separator + FILE_WILDCARD);
+		globOptr.evaluate(cmd);
+		assertEquals(expected, cmd);
 	}
 	
 	@Test
 	public void testGlobMutiplePath() throws AbstractApplicationException, ShellException {
-		expected = new String[] {FOLDER1_FILE1, FOLDER1_FILE2, FOLDER1_FOLDER_WS, FOLDER2_FILE1, FOLDER2_FILE2};
-		input = new String[] {FOLDER1 + File.separator + "*", FOLDER2 + File.separator + "*"};
-		output = globOptr.evaluate(input);
-		assertArrayEquals(expected, output);
+		expected = getExpectedCmdStr(FOLDER1_FILE1, FOLDER1_FILE2, FOLDER1_FOLDER_WS, FOLDER2_FILE1, FOLDER2_FILE2);
+		cmd = new CommandString(FOLDER1 + File.separator + "*" + STR_SPACE + FOLDER2 + File.separator + "*");
+		globOptr.evaluate(cmd);
+		assertEquals(expected, cmd);
 	}
 	
 	@Test
 	public void testGlobExistingAndNonExistingFile() throws AbstractApplicationException, ShellException {
-		expected = new String[] {FILE_W_SPACES, FILE1, FILE2, FILE_NONEXISTENT};
-		input = new String[] {"file*", FILE_NONEXISTENT};
-		output = globOptr.evaluate(input);
-		assertArrayEquals(expected, output);
+		expected = getExpectedCmdStr(FILE_W_SPACES, FILE1, FILE2, FILE_NONEXISTENT);
+		expected.setCharEscapedRange(expected.length() - FILE_NONEXISTENT.length(), expected.length(), false);
+		cmd = new CommandString(FILE_WILDCARD + STR_SPACE + FILE_NONEXISTENT);
+		globOptr.evaluate(cmd);
+		assertEquals(expected, cmd);
 	}
 	
 	@Test
-	public void testGlobRemoveQuote() throws AbstractApplicationException, ShellException {
-		expected = new String[] {FILE_W_SPACES};
-		input = new String[] {"\"file name with\"*"};
-		output = globOptr.evaluate(input);
-		assertArrayEquals(expected, output);
+	public void testGlobEscapedWildcard() throws AbstractApplicationException, ShellException {
+		expected = getExpectedCmdStr(FILE_WILDCARD);
+		cmd = new CommandString(FILE_WILDCARD);
+		cmd.setCharEscaped(0, true, true, true, true, true);
+		globOptr.evaluate(cmd);
+		assertEquals(expected, cmd);
+	}
+	
+
+	@Test
+	public void testGlobFileNameWithSpaces() throws AbstractApplicationException, ShellException {
+		expected = getExpectedCmdStr(FILE_W_SPACES);
+		cmd = new CommandString("file name with*");
+		cmd.setCharEscaped(0, true, true, true, true, true, true, true, true, true, true, true, true, true, true, false);
+		globOptr.evaluate(cmd);
+		assertEquals(expected, cmd);
 	}
 
 	@Test
 	public void testInvalidGlobNull() throws AbstractApplicationException, ShellException {
-		input = new String[] {null};
-		
 		thrown.expect(ShellException.class);
         thrown.expectMessage("shell: Null Pointer Exception");
-
-		globOptr.evaluate(input);
+		globOptr.evaluate(null);
+	}
+	
+	private CommandString getExpectedCmdStr(String... strings) {
+		CommandString cmdStr = new CommandString();
+		int startIndex = 0;
+		for (int i = 0; i < strings.length; i++) {
+			cmdStr.insertStringAt(startIndex, strings[i]);
+			cmdStr.setCharEscapedRange(startIndex, cmdStr.length(), true);
+			startIndex = cmdStr.length();
+			
+			if (i < strings.length - 1) {
+				cmdStr.insertStringAt(startIndex, " ");
+				cmdStr.setCharEscapedRange(startIndex, cmdStr.length(), false);
+				startIndex = cmdStr.length();
+			}
+		}
+		return cmdStr;
 	}
 }
