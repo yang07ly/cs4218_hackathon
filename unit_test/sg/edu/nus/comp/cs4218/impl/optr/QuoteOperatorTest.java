9503427449fe1e2d091da1bdab7284dd60a587c5
Tan Qin Hui <destqh@hotmail.com>
2018-03-23 10:18:26 +0800

    update quote test to use CommandString equals.

--- unit_test/sg/edu/nus/comp/cs4218/impl/optr/QuoteOperatorTest.java
+++ unit_test/sg/edu/nus/comp/cs4218/impl/optr/QuoteOperatorTest.java
@@ -1,6 +1,5 @@
 package sg.edu.nus.comp.cs4218.impl.optr;
 
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
@@ -18,9 +17,7 @@ public class QuoteOperatorTest {
 	private static final String NULL_PTR_EXP = "shell: Null Pointer Exception";
 	
 	private QuoteOperator quoteOptr;
-	private CommandString cmd;
-	private String expectedStr;
-	private Boolean[] expectedBool;
+	private CommandString cmd, expected;
 	
 	@Rule
     public ExpectedException thrown = ExpectedException.none();
@@ -29,212 +26,177 @@ public class QuoteOperatorTest {
 	public void setup() {
 		quoteOptr = new QuoteOperator();
 		cmd = new CommandString();
-		expectedStr = "";
-		expectedBool = new Boolean[0];
+		expected = new CommandString();
 	}
 
 	@Test
 	public void testEvaluateNoQuotes() throws AbstractApplicationException, ShellException {
-		expectedStr = "message";
-		expectedBool = new Boolean[] {false, false, false, false, false, false, false};
+		expected = new CommandString("message");
+		expected.setCharEscaped(0, false, false, false, false, false, false, false);
 		
 		cmd = new CommandString("message");
 		quoteOptr.evaluate(cmd);
-		
-		assertEquals(expectedStr, cmd.toString());
-		assertArrayEquals(expectedBool, cmd.toBoolArray());
+		assertEquals(expected, cmd);
 	}
 
 	@Test
 	public void testEvaluateSQClosed() throws AbstractApplicationException, ShellException {
-		expectedStr = "SQ message";
-		expectedBool = new Boolean[] {true, true, true, true, true, true, true, true, true, true};
+		expected = new CommandString("SQ message");
+		expected.setCharEscaped(0, true, true, true, true, true, true, true, true, true, true);
 		
 		cmd = new CommandString("'SQ message'");
 		quoteOptr.evaluate(cmd);
-		
-		assertEquals(expectedStr, cmd.toString());
-		assertArrayEquals(expectedBool, cmd.toBoolArray());
+		assertEquals(expected, cmd);
 	}
 
 	@Test
 	public void testEvaluateSQMultiple() throws AbstractApplicationException, ShellException {
-		expectedStr = "IN OUT IN";
-		expectedBool = new Boolean[] {true, true, false, false, false, false, false, true, true};
+		expected = new CommandString("IN OUT IN");
+		expected.setCharEscaped(0, true, true, false, false, false, false, false, true, true);
 		
 		cmd = new CommandString("'IN' OUT 'IN'");
 		quoteOptr.evaluate(cmd);
-		
-		assertEquals(expectedStr, cmd.toString());
-		assertArrayEquals(expectedBool, cmd.toBoolArray());
+		assertEquals(expected, cmd);
 	}
 	
 	@Test
 	public void testEvaluateSQWithinWord() throws AbstractApplicationException, ShellException {
-		expectedStr = "OI IO";
-		expectedBool = new Boolean[] {false, true, true, true, false};
+		expected = new CommandString("OI IO");
+		expected.setCharEscaped(0, false, true, true, true, false);
 		
 		cmd = new CommandString("O'I I'O");
 		quoteOptr.evaluate(cmd);
-		
-		assertEquals(expectedStr, cmd.toString());
-		assertArrayEquals(expectedBool, cmd.toBoolArray());
+		assertEquals(expected, cmd);
 	}
 	
 	@Test
 	public void testEvaluateDQClosed() throws AbstractApplicationException, ShellException {
-		expectedStr = "DQ message";
-		expectedBool = new Boolean[] {true, true, true, true, true, true, true, true, true, true};
+		expected = new CommandString("DQ message");
+		expected.setCharEscaped(0, true, true, true, true, true, true, true, true, true, true);
 		
 		cmd = new CommandString("\"DQ message\"");
 		quoteOptr.evaluate(cmd);
-		
-		assertEquals(expectedStr, cmd.toString());
-		assertArrayEquals(expectedBool, cmd.toBoolArray());
+		assertEquals(expected, cmd);
 	}
 
 	@Test
 	public void testEvaluateDQMultiple() throws AbstractApplicationException, ShellException {
-		expectedStr = "IN OUT IN";
-		expectedBool = new Boolean[] {true, true, false, false, false, false, false, true, true};
+		expected = new CommandString("IN OUT IN");
+		expected.setCharEscaped(0, true, true, false, false, false, false, false, true, true);
 		
 		cmd = new CommandString("\"IN\" OUT \"IN\"");
 		quoteOptr.evaluate(cmd);
-		
-		assertEquals(expectedStr, cmd.toString());
-		assertArrayEquals(expectedBool, cmd.toBoolArray());
+		assertEquals(expected, cmd);
 	}
 	
 	@Test
 	public void testEvaluateDQWithinWord() throws AbstractApplicationException, ShellException {
-		expectedStr = "OI IO";
-		expectedBool = new Boolean[] {false, true, true, true, false};
+		expected = new CommandString("OI IO");
+		expected.setCharEscaped(0, false, true, true, true, false);
 		
 		cmd = new CommandString("O\"I I\"O");
 		quoteOptr.evaluate(cmd);
-		
-		assertEquals(expectedStr, cmd.toString());
-		assertArrayEquals(expectedBool, cmd.toBoolArray());
+		assertEquals(expected, cmd);
 	}
 	
 	@Test
 	public void testEvaluateBQClosed() throws AbstractApplicationException, ShellException {
-		expectedStr = "`BQ message`";
-		expectedBool = new Boolean[] {false, true, true, true, true, true, true, true, true, true, true, false};
+		expected = new CommandString("`BQ message`");
+		expected.setCharEscaped(0, false, true, true, true, true, true, true, true, true, true, true, false);
 		
 		cmd = new CommandString("`BQ message`");
 		quoteOptr.evaluate(cmd);
-		
-		assertEquals(expectedStr, cmd.toString());
-		assertArrayEquals(expectedBool, cmd.toBoolArray());
+		assertEquals(expected, cmd);
 	}
 
 	@Test
 	public void testEvaluateBQMultiple() throws AbstractApplicationException, ShellException {
-		expectedStr = "`IN` OUT `IN`";
-		expectedBool = new Boolean[] {false, true, true, false, false, false, false, false, false, false, true, true, false};
+		expected = new CommandString("`IN` OUT `IN`");
+		expected.setCharEscaped(0, false, true, true, false, false, false, false, false, false, false, true, true, false);
 		
 		cmd = new CommandString("`IN` OUT `IN`");
 		quoteOptr.evaluate(cmd);
-		
-		assertEquals(expectedStr, cmd.toString());
-		assertArrayEquals(expectedBool, cmd.toBoolArray());
+		assertEquals(expected, cmd);
 	}
 	
 	@Test
 	public void testEvaluateBQWithinWord() throws AbstractApplicationException, ShellException {
-		expectedStr = "O`I I`O";
-		expectedBool = new Boolean[] {false, false, true, true, true, false, false};
+		expected = new CommandString("O`I I`O");
+		expected.setCharEscaped(0, false, false, true, true, true, false, false);
 		
 		cmd = new CommandString("O`I I`O");
 		quoteOptr.evaluate(cmd);
-		
-		assertEquals(expectedStr, cmd.toString());
-		assertArrayEquals(expectedBool, cmd.toBoolArray());
+		assertEquals(expected, cmd);
 	}
 
 	@Test
 	public void testEvaluateMultipleQuotes() throws AbstractApplicationException, ShellException {
-		expectedStr = "SQ DQ `BQ`";
-		expectedBool = new Boolean[] {true, true, false, true, true, false, false, true, true, false};
+		expected = new CommandString("SQ DQ `BQ`");
+		expected.setCharEscaped(0, true, true, false, true, true, false, false, true, true, false);
 		
 		cmd = new CommandString("'SQ' \"DQ\" `BQ`");
 		quoteOptr.evaluate(cmd);
-		
-		assertEquals(expectedStr, cmd.toString());
-		assertArrayEquals(expectedBool, cmd.toBoolArray());
+		assertEquals(expected, cmd);
 	}
 
 	@Test
 	public void testEvaluateDQWithinSQ() throws AbstractApplicationException, ShellException {
-		expectedStr = "DQ \"IN\" SQ";
-		expectedBool = new Boolean[] {true, true, true, true, true, true, true, true, true, true};
+		expected = new CommandString("DQ \"IN\" SQ");
+		expected.setCharEscaped(0, true, true, true, true, true, true, true, true, true, true);
 		
 		cmd = new CommandString("'DQ \"IN\" SQ'");
 		quoteOptr.evaluate(cmd);
-		
-		assertEquals(expectedStr, cmd.toString());
-		assertArrayEquals(expectedBool, cmd.toBoolArray());
+		assertEquals(expected, cmd);
 	}
 	
 	@Test
 	public void testEvaluateBQWithinSQ() throws AbstractApplicationException, ShellException {
-		expectedStr = "BQ `IN` SQ";
-		expectedBool = new Boolean[] {true, true, true, true, true, true, true, true, true, true};
+		expected = new CommandString("BQ `IN` SQ");
+		expected.setCharEscaped(0, true, true, true, true, true, true, true, true, true, true);
 		
 		cmd = new CommandString("'BQ `IN` SQ'");
 		quoteOptr.evaluate(cmd);
-		
-		assertEquals(expectedStr, cmd.toString());
-		assertArrayEquals(expectedBool, cmd.toBoolArray());
+		assertEquals(expected, cmd);
 	}
 
 	@Test
 	public void testEvaluateSQWithinDQ() throws AbstractApplicationException, ShellException {
-		expectedStr = "SQ 'IN' DQ";
-		expectedBool = new Boolean[] {true, true, true, true, true, true, true, true, true, true};
+		expected = new CommandString("SQ 'IN' DQ");
+		expected.setCharEscaped(0, true, true, true, true, true, true, true, true, true, true);
 		
 		cmd = new CommandString("\"SQ 'IN' DQ\"");
 		quoteOptr.evaluate(cmd);
-		
-		assertEquals(expectedStr, cmd.toString());
-		assertArrayEquals(expectedBool, cmd.toBoolArray());
+		assertEquals(expected, cmd);
 	}
 
 	@Test
 	public void testEvaluateBQWithinDQ() throws AbstractApplicationException, ShellException {
-		expectedStr = "BQ `IN` DQ";
-		expectedBool = new Boolean[] {true, true, true, false, false, false, false, true, true, true};
+		expected = new CommandString("BQ `IN` DQ");
+		expected.setCharEscaped(0, true, true, true, false, false, false, false, true, true, true);
 		
 		cmd = new CommandString("\"BQ `IN` DQ\"");
 		quoteOptr.evaluate(cmd);
-		
-		assertEquals(expectedStr, cmd.toString());
-		assertArrayEquals(expectedBool, cmd.toBoolArray());
+		assertEquals(expected, cmd);
 	}
 	
 	@Test
 	public void testEvaluateSQWithinBQ() throws AbstractApplicationException, ShellException {
-		expectedStr = "`SQ 'IN' BQ`";
-		expectedBool = new Boolean[] {false, true, true, true, true, true, true, true, true, true, true, false};
+		expected = new CommandString("`SQ 'IN' BQ`");
+		expected.setCharEscaped(0, false, true, true, true, true, true, true, true, true, true, true, false);
 		
 		cmd = new CommandString("`SQ 'IN' BQ`");
 		quoteOptr.evaluate(cmd);
-		
-		assertEquals(expectedStr, cmd.toString());
-		assertArrayEquals(expectedBool, cmd.toBoolArray());
+		assertEquals(expected, cmd);
 	}
 	
 	@Test
 	public void testEvaluateDQWithinBQ() throws AbstractApplicationException, ShellException {
-		expectedStr = "`DQ \"IN\" BQ`";
-		expectedBool = new Boolean[] {false, true, true, true, true, true, true, true, true, true, true, false};
+		expected = new CommandString("`DQ \"IN\" BQ`");
+		expected.setCharEscaped(0, false, true, true, true, true, true, true, true, true, true, true, false);
 		
 		cmd = new CommandString("`DQ \"IN\" BQ`");
 		quoteOptr.evaluate(cmd);
-		
-		assertEquals(expectedStr, cmd.toString());
-		assertArrayEquals(expectedBool, cmd.toBoolArray());
+		assertEquals(expected, cmd);
 	}
 	
 	@Test
