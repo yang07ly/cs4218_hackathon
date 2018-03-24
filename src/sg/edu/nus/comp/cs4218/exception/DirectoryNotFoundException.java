078dc219283a679f3bb8cc7f01e42ecc93ccad03
Tan Qin Hui <destqh@hotmail.com>
2018-03-24 18:05:35 +0800

    remove unnecessary exception class.

--- src/sg/edu/nus/comp/cs4218/exception/DirectoryNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package sg.edu.nus.comp.cs4218.exception;
-
-public class DirectoryNotFoundException extends Exception {
-	private static final long serialVersionUID = 9208237916723540057L;
-	
-	public DirectoryNotFoundException(String message) {
-		super(message);
-	}
-}
